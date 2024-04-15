933e34a9fed4d52cb3d43f53b3c126097ea77955
MiguelMiranda534 <127416119+MiguelMiranda534@users.noreply.github.com>
2024-04-16 00:31:07 +0100

    Botão Criar atividades

--- Back-Office/js/atividades.js
+++ Back-Office/js/atividades.js
@@ -1,29 +1,14 @@
 $(document).ready(function() {
-    var table1 = $('#atividades').DataTable({
-        "ajax": {
-            "url": "js/dados.txt",
-            "dataSrc": "dados"
-        },
+    var table = $('#atividades').DataTable({
+        "ajax": "js/data.txt",
         "columns": [
-            { "data": "Codigo" },
-            { "data": "Nome" },
-            { "data": "Atividade" },
-            { "data": "Organizador" },
-            { "data": "Local" },
-            { "data": "Status" },
-            { "data": "Data" },
-            {
-                "className": 'details-control',
-                "orderable": false,
-                "data": null,
-                "defaultContent": ''
-            }
-        ],
-        "order": [[1, 'asc']],
-        "paging": false,
-        "ordering": true,
-        "info": false,
-        "filter": false,
-        
+          { "data": "Codigo" },
+          { "data": "Nome" },
+          { "data": "Atividade" },
+          { "data": "Organizador" },
+          { "data": "Local" },
+          { "data": "Estado" },
+          { "data": "Data" },
+        ]
     });
 });
