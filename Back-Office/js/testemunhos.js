ddf483ca6fab09392d9685c60c94354ff902c088
mariananeiva <118939544+mariananeiva@users.noreply.github.com>
2024-05-02 22:32:50 +0100

    testemunhos

--- Back-Office/js/testemunhos.js
+++ Back-Office/js/testemunhos.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-    var table = $('#testemunhos').DataTable({
-        "ajax": "js/testemunhos.txt",
-        "columns": [
+$(document).ready(function () {
+  var table = $('#testemunhos').DataTable({
+      "ajax": "js/testemunhos.txt",
+      "columns": [
           { "data": "Nome" },
           { "data": "DataPublicacao" },
-        ]
-    });
+          {
+          "data": null,
+          "render": function (data, type, full, meta) {
+              return '<i class="mdi mdi-pencil editar-icon" style="font-size: 24px; margin-right: 15px; cursor: pointer;"></i>' +
+              '<i class="mdi mdi-delete apagar-icon" style="font-size: 24px;"></i>';
+          }
+          }
+      ]
+  });
 });
