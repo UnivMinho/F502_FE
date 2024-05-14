d0957d374cf76a27eb229772f97e7dad05c7876e
19Rafa21 <rafaalves1921@gmail.com>
2024-05-15 00:32:40 +0100

    botaoGoogle

--- Front-Office/js/login.js
+++ Front-Office/js/login.js
@@ -1,18 +1,26 @@
 function handleCredentialResponse(response) {
-
+    const data = jwt_decode(response.credential)
+    console.log(data)
   }
-
+  
   window.onload = function () {
 
     google.accounts.id.initialize({
-      client_id: "10382157412-gqc4mt336cl69psamqja4v95knivl0p9.apps.googleusercontent.com", //COLOCAR CLIENT ID DAS CREDENCIAIS
+      client_id: "10382157412-gqc4mt336cl69psamqja4v95knivl0p9.apps.googleusercontent.com",
       callback: handleCredentialResponse
     });
 
     google.accounts.id.renderButton(
       document.getElementById("buttonGoogle"),
-      { theme: "outline", size: "large" }  // customization attributes
+      { type:"standard",
+        shape:"pill",
+        theme:"filled_black",
+        text:"signin_with",
+        size:"large",
+        logo_alignment:"left",
+        width:"270"
+      }  // customization attributes
     );
 
     google.accounts.id.prompt(); // also display the One Tap dialog
-  }
+  };
